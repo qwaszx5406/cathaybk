876de7d7e54ecb5a772d9831c329c586278660d7
LU,JUN-YU <35597521+qwaszx5406@users.noreply.github.com>
2021-11-30 14:20:13 +0800

    test4

---
+++ src/CathaybkAppApiServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cathaybk\Api;
+
+use Illuminate\Support\ServiceProvider;
+
+class CathaybkAppApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+		
+		$configPath = __DIR__ . '/../config/cathaybk.php';
+        $this->mergeConfigFrom($configPath, 'cathaybk');
+		
+		$this->app->singleton('OpenWallet', function () {
+            return new OpenWallet();
+        });
+		
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+		$source = realpath($raw = __DIR__ . '/../config/cathaybk.php') ?: $raw;
+        $this->publishes([
+            $source => config_path('cathaybk.php'),
+        ]);
+    }
+}
